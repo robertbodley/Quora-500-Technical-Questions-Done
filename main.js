4bd8ee0e5d2fe02758616e58dfc65a183808143b
Robert Bodley <rpbodley@gmail.com>
2017-05-03 16:34:30 +0200

    added count

--- main.js
+++ main.js
@@ -1,20 +1,28 @@
 $(document).ready(function() {
+	console.log("running");
 	var question = 1;
+	var count = 0;
 	$(".qlink_container").each(function( index ) {
 		var localStorageCurrently = localStorage.getItem("questionComplete");
-		if(localStorageCurrently.indexOf(question + ",") !== -1) {
+		if(localStorageCurrently!=null && localStorageCurrently.indexOf(question + ",") !== -1) {
 			var html = "<input type='checkbox' checked name='questionNumber' value='" + question + "'> " + $(this).html();
+			count++;
 		} else {
 			var html = "<input type='checkbox' name='questionNumber' value='" + question + "'> " + $(this).html();
 		}
+
+			
 		$(this).html(html);
 		question++;
 	});
+
+	$('.board_item_title p.qtext_para').text("500 Data Structures and Algorithms practice problems and their solutions (" + count + "/500)");
 
 
 	//handles storage in localStorage to remeber what has been completed
 	$('input[type="checkbox"][name="questionNumber"]').change(function() {
 		if(this.checked) {
+			count++;
 			var localStorageCurrently = localStorage.getItem("questionComplete");
 			var questionNumber = $(this).val();
 
@@ -22,12 +30,19 @@ $(document).ready(function() {
 
 			localStorage.setItem("questionComplete", questionComplete);
 		} else {
+			count--;
+
 			var localStorageCurrently = localStorage.getItem("questionComplete");
 			var questionNumber = $(this).val();
 
 			var questionComplete = localStorageCurrently.replace(questionNumber+",", "");
 			localStorage.setItem("questionComplete", questionComplete);
+
+
 		}
+		
+		$('.board_item_title p.qtext_para').text("500 Data Structures and Algorithms practice problems and their solutions (" + count + "/500)");
+
 		console.log(localStorage.getItem("questionComplete"));
 	});
 });
